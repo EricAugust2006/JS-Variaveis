380a97c6b657d022075a57af218ebe14ef06dd17
Eric <ericaugust2006@gmail.com>
2024-02-26 23:33:38 -0300

    Atividade feita

---
+++ index.js
@@ -0,0 +1,51 @@
+// console.log("Eu Funciono!");
+// console.log("Arquivo de script interno");
+
+// let meuNumero = 20;
+// let outroNumero = 15;
+// let meuNome = "Eric Augusto"
+// let idade = 18;
+// console.log(meuNumero + outroNumero);
+
+// meuNumero = 40;
+// outroNumero = 50;
+
+// console.log(`Meu número é: ${meuNumero + outroNumero}`);
+
+// console.log(`Meu nome é ${meuNome} e tenho ${idade} anos.`);
+
+// console.log(typeof meuNome + " " + typeof idade);
+
+console.log("Exercício 1: Faça um programa que exiba um decimal, um inteiro e uma string utilizando o console.log().")
+let numeroDecimal = 10.5;
+let numeroInteiro = 30;
+let palavraString = "Frase";
+
+console.log(`Isso é um numero decimal: ${numeroDecimal}`);
+console.log(`Isso é um numero inteiro: ${numeroInteiro}`);
+console.log(`Isso é uma palavra string: ${palavraString}`);     
+
+console.log("Exercício 2: Declare uma variável, atribua um número, use o método typeof para mostrar na tela a palavra 'number'.");
+let número = 18;
+console.log(typeof variável);
+
+console.log("Exercício 3: Declare uma variável, atribua um valor booleano, use o método typeof para mostrar na tela a palavra 'boolean'.")
+let booleana = true;
+console.log(typeof booleana);
+
+console.log("Exercício 4: Declare uma variável, atribua uma string, use o método typeof para mostrar na tela a palavra 'string'.")
+let string = "Frase";
+console.log(typeof string);
+
+console.log("Exercício 5: Faça um programa que exiba duas variáveis, uma com atribuição true e outra false");
+let variávelTrue = true;
+let variávelFalse = false;
+console.log(variávelTrue + " " + variávelFalse);
+
+console.log("Exercício 6: Usando uma variável, e o console.log(), mostre no console o valor 'null'")
+let variávelNull = null;
+console.log(variávelNull);
+
+console.log("Exercício 7: Usando uma variável, e o console.log(), mostre no console o valor 'undefined'")
+let variávelUndefined;
+console.log(variávelUndefined);
